895ee404fcc11defd301399ed85a38cfec1783e2
mnmccoy95 <mnmccoy95@gmail.com>
2020-11-13 09:23:32 -0600

    the moose asks

--- Program.cs
+++ Program.cs
@@ -13,38 +13,63 @@ namespace EnthusiasticMoose
             // Let the moose speak!
             MooseSays("H I, I'M  E N T H U S I A S T I C !");
             MooseSays("I really am enthusiastic");
+
+            // Ask a question
+            bool isTrue = MooseAsks("Is Canada real?");
+            Console.WriteLine(isTrue);
         }
 
         static void MooseSays(string message)
         {
             Console.WriteLine($@"
-                                       _.--^^^--,
-                                    .'          `\
-  .-^^^^^^-.                      .'              |
- /          '.                   /            .-._/
-|             `.                |             |
- \              \          .-._ |          _   \
-  `^^'-.         \_.-.     \   `          ( \__/
-        |             )     '=.       .,   \
-       /             (         \     /  \  /
-     /`               `\        |   /    `'
-     '..-`\        _.-. `\ _.__/   .=.
-          |  _    / \  '.-`    `-.'  /
-          \_/ |  |   './ _     _  \.'
-               '-'    | /       \ |
-                      |  .-. .-.  |
-                      \ / o| |o \ /
-                       |   / \   |    {message}
-                      / `^`   `^` \
-                     /             \
-                    | '._.'         \
-                    |  /             |
-                     \ |             |
-                      ||    _    _   /
-                      /|\  (_\  /_) /
-                      \ \'._  ` '_.'
-                       `^^` `^^^`
+                                                _.--^^^--,
+                                                .'          `\
+            .-^^^^^^-.                      .'              |
+            /          '.                   /            .-._/
+            |             `.                |             |
+            \              \          .-._ |          _   \
+            `^^'-.         \_.-.     \   `          ( \__/
+                    |             )     '=.       .,   \
+                /             (         \     /  \  /
+                /`               `\        |   /    `'
+                '..-`\        _.-. `\ _.__/   .=.
+                    |  _    / \  '.-`    `-.'  /
+                    \_/ |  |   './ _     _  \.'
+                        '-'    | /       \ |
+                                |  .-. .-.  |
+                                \ / o| |o \ /
+                                |   / \   |    {message}
+                                / `^`   `^` \
+                                /             \
+                                | '._.'         \
+                                |  /             |
+                                \ |             |
+                                ||    _    _   /
+                                /|\  (_\  /_) /
+                                \ \'._  ` '_.'
+                                `^^` `^^^`
             ");
+        }
+
+        static bool MooseAsks(string question)
+        {
+            Console.Write($"{question} (Y/N): ");
+            string answer = Console.ReadLine().ToLower();
+
+            while (answer != "y" && answer != "n")
+            {
+                Console.Write($"{question} (Y/N): ");
+                answer = Console.ReadLine().ToLower();
+            }
+
+            if (answer == "y")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
